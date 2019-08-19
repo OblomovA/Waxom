2f00c43eb4be4ddb0b9cdb0e6e7a6663b69659c8
OblomovA <nastya.oblomskaya@gmail.com>
2019-08-19 16:04:37 +0300

    Added files css and js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-$(document).ready(function () {
-var swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows : true,
-    }),
+// $(document).ready(function () {
+// var swiper = new Swiper('.swiper-container', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: 'auto',
+//     coverflowEffect: {
+//       rotate: 50,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 1,
+//       slideShadows : true
+//     });
+//   });
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
